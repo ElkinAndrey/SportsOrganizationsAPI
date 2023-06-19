66d7406b1c65d9d261c0385192a4d926be18e8be
ElkinAndrey <andreyridbir@gmail.com>
2023-06-19 17:58:50 +0300

    Добавил хранилище работающее при помощи средств EF Core для работы с простыми сущностями (справочниками)

---
+++ SportsOrganizationsAPI/SportsOrganizationsAPI.Persistence/Repositories/EFHandBookRepository.cs
@@ -0,0 +1,30 @@
+﻿using SportsOrganizationsAPI.Persistence.Abstractions;
+
+namespace SportsOrganizationsAPI.Persistence.Repositories
+{
+    /// <summary>
+    /// Репозиторий, отвечающий за работу с простыми сущностями
+    /// </summary>
+    /// <remarks>
+    /// Работает при помощи средств Entity Framework Core
+    /// </remarks>
+    public class EFHandBookRepository : IHandBookRepository
+    {
+        /// <summary>
+        /// Контекст базы данных
+        /// </summary>
+        private readonly ApplicationDbContext _context;
+
+        /// <summary>
+        /// Репозиторий, отвечающий за работу с простыми сущностями
+        /// </summary>
+        /// <remarks>
+        /// Работает при помощи средств Entity Framework Core
+        /// </remarks>
+        /// <param name="context">Контекст базы данных</param>
+        public EFHandBookRepository(ApplicationDbContext context) 
+        { 
+            _context = context; 
+        }
+    }
+}
---
+++ SportsOrganizationsAPI/SportsOrganizationsAPI.Persistence/Repositories/RepositoryManager.cs
@@ -0,0 +1,28 @@
+﻿using SportsOrganizationsAPI.Persistence.Abstractions;
+
+namespace SportsOrganizationsAPI.Persistence.Repositories
+{
+    /// <summary>
+    /// Репозиторий для работы с остальными репозиториями
+    /// </summary>
+    public class RepositoryManager : IRepositoryManager
+    {
+        /// <summary>
+        /// Репозиторий, отвечающий за работу с простыми сущностями
+        /// </summary>
+        private readonly IHandBookRepository handBookRepository;
+
+        /// <summary>
+        /// Репозиторий для работы с остальными репозиториями
+        /// </summary>
+        public RepositoryManager(ApplicationDbContext context)
+        {
+            handBookRepository = new EFHandBookRepository(context);
+        }
+
+        /// <summary>
+        /// Репозиторий, отвечающий за работу с простыми сущностями
+        /// </summary>
+        public IHandBookRepository HandBookRepository => handBookRepository;
+    }
+}
