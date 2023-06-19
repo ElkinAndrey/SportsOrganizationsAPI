382f935afffa9f56a6fc0f680b5411519fc69b1b
ElkinAndrey <andreyridbir@gmail.com>
2023-06-19 17:44:53 +0300

    Добавил интерфейс хранилища для работы с простыми сущностями (справочниками)

---
+++ SportsOrganizationsAPI/SportsOrganizationsAPI.Persistence/Abstractions/IHandBookRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsOrganizationsAPI.Persistence.Abstractions
+{
+    /// <summary>
+    /// Репозиторий, отвечающий за работу с простыми сущностями
+    /// </summary>
+    public interface IHandBookRepository
+    {
+    }
+}
---
+++ SportsOrganizationsAPI/SportsOrganizationsAPI.Persistence/Abstractions/IRepositoryManager.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsOrganizationsAPI.Persistence.Abstractions
+{
+    /// <summary>
+    /// Менеджер репозиториев
+    /// </summary>
+    public interface IRepositoryManager
+    {
+        /// <summary>
+        /// Репозиторий, отвечающий за работу с простыми сущностями
+        /// </summary>
+        IHandBookRepository HandBookRepository { get; }
+    }
+}
